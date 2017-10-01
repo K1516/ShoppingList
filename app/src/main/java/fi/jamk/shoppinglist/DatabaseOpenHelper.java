a2ab85dc0b1727f63f7662d99c0c4dc2fdcccac5
Risto Laine <k1516@student.jamk.fi>
2017-10-02 00:22:19 +0300

    Initial commit

---
+++ app/src/main/java/fi/jamk/shoppinglist/DatabaseOpenHelper.java
@@ -0,0 +1,47 @@
+package fi.jamk.shoppinglist;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Risto on 1.10.2017.
+ */
+
+public class DatabaseOpenHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "ShoppingList";
+    private final String DATABASE_TABLE = "ShoppingList";
+    private final String ITEM = "item";
+    private final String COUNT = "count";
+    private final String PRICE = "price";
+
+    public DatabaseOpenHelper(Context context) {
+        // Context, database name, optional cursor factory, database version
+        super(context, DATABASE_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // create a new table
+        db.execSQL("CREATE TABLE "+DATABASE_TABLE+" (_id INTEGER PRIMARY KEY AUTOINCREMENT, "+ITEM+" TEXT, "+COUNT+" REAL, "+PRICE+" REAL);");
+        // create sample data
+        ContentValues values = new ContentValues();
+        values.put(ITEM, "Maito");
+        values.put(COUNT, 2);
+        values.put(PRICE, 1.02);
+        // insert data to database, name of table, "Nullcolumnhack", values
+        db.insert(DATABASE_TABLE, null, values);
+        // a more data...
+        values.put(ITEM, "Kahvi");
+        values.put(COUNT, 3);
+        values.put(PRICE, 4.53);
+        db.insert(DATABASE_TABLE, null, values);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+DATABASE_TABLE);
+        onCreate(db);
+    }
+}
